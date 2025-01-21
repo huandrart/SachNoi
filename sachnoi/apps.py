4a46c31fd166e815e2c511af39d4ddc9f36d2574
Minh Tri <minhtrivo2005gg@gmail.com>
2025-01-22 00:12:42 +0700

    fix

--- sachnoi/apps.py
+++ sachnoi/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+# from sachnoi import signals
 
 class SachnoiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'sachnoi'
+    def ready(self):
+        import sachnoi.signals
