9446cd68b712f1d9e259b92e969680f609b99272
devil-dev-game <huandbne@gmail.com>
2025-01-22 21:25:55 +0700

    update reset mk

--- sachnoi/urls.py
+++ sachnoi/urls.py
@@ -11,8 +11,5 @@ urlpatterns = [
     path('logout/',views.logoutPage, name='logout'),
     path('signup.html',views.signup, name='signup'),
     path('forgot-password/', viewfp.forgot_password, name='forgot_password'),
-    path('enter-otp-code/', viewfp.enter_OTP_code, name='enter_otp_code'),
-    path('reset-password/', viewfp.reset_password, name='reset_password'),
     path('search/', views.search_results, name='search_results'),
-    path('book/<int:pk>/', views.book_detail, name='book_detail'),
 ]
